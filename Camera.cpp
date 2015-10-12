2dd47bd925dda86258051584c13727c728f863a1
Christopher Zhang <chris@Christophers-MacBook-Air.local>
2015-10-11 23:56:11 -0400

    Model camera data with position, axes, and direction.

---
+++ Camera.cpp
@@ -0,0 +1,44 @@
+#include "Camera.h"
+
+Camera::Camera() {
+
+    position = Vector3D(0,0,0);
+    direction = Vector3D(0,0,1);
+    right = Vector3D(0,0,0);
+    down = Vector3D(0,0,0);
+
+}
+
+Camera::Camera(Vector3D position, Vector3D direction, Vector3D right,
+               Vector3D down) {
+
+    this->position = position;
+    this->direction = direction;
+    this->right = right;
+    this->down = down;
+
+}
+
+Vector3D Camera::getPosition() const {
+
+    return position;
+
+}
+
+Vector3D Camera::getDirection() const {
+
+    return direction;
+
+}
+
+Vector3D Camera::getRight() const {
+
+    return right;
+
+}
+
+Vector3D Camera::getDown() const {
+
+    return down;
+
+}
---
+++ Camera.h
@@ -0,0 +1,28 @@
+#ifndef __CAMERA__
+#define __CAMERA__
+
+#include "Vector3D.h"
+
+class Camera {
+
+    Vector3D position;
+    Vector3D direction;
+
+    // For coordinate system
+    Vector3D right;
+    Vector3D down;
+
+    public:
+
+        Camera();
+        Camera(Vector3D position, Vector3D direction, Vector3D right,
+               Vector3D down);
+
+        Vector3D getPosition() const;
+        Vector3D getDirection() const;
+        Vector3D getRight() const;
+        Vector3D getDown() const;
+
+};
+
+#endif
