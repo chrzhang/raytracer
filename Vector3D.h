8e1d0df974f584cca6c436bc1b62999e57c4469d
Christopher Zhang <chris@Christophers-MacBook-Air.local>
2015-10-12 00:12:03 -0400

    Consider the scaling methods as friend functions.

--- Vector3D.h
+++ Vector3D.h
@@ -1,6 +1,8 @@
 #ifndef __VECTOR3D__
 #define __VECTOR3D__
 
+#include <iostream>
+
 class Vector3D {
 
     double x, y, z;
@@ -18,6 +20,9 @@ class Vector3D {
         double dotProduct(const Vector3D & v) const;
         Vector3D crossProduct(const Vector3D & v) const;
         friend Vector3D operator+(const Vector3D & lhs, const Vector3D & rhs);
+        friend Vector3D operator*(const Vector3D & v, double scale);
+        friend Vector3D operator*(double scale, const Vector3D & v);
+        friend std::ostream & operator<<(std::ostream & os, const Vector3D & v);
 
 };
 
