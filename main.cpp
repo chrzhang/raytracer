962974243c63b46f52f190721bd9fcd9f463d336
Christopher Zhang <chris@Christophers-MacBook-Air.local>
2015-10-11 22:38:45 -0400

    Add a border and control range of drawn pixels.

--- main.cpp
+++ main.cpp
@@ -33,21 +33,20 @@ int main() {
         for (size_t y = 0; y < height; ++y) {
             pixelindex = y * width + x;
             // Return color
-/*
-            size_t lowerx = rand() % 200;
-            size_t upperx = 200 + (rand() % 200);
-            size_t lowery = rand() % 200;
-            size_t uppery = 200 + (rand() % 80);
-*/
-           // if ((x > lowerx && x < upperx) && (y > lowery && y < uppery)) {
+
+            size_t lowerx = 200;
+            size_t upperx = 440;
+            size_t lowery = 200;
+            size_t uppery = 280;
+            if ((x > lowerx && x < upperx) && (y > lowery && y < uppery)) {
                 pixels[pixelindex].r = rand() % 256;
                 pixels[pixelindex].g = rand() % 256;
                 pixels[pixelindex].b = rand() % 256;
-          /*  } else {
+            } else {
                 pixels[pixelindex].r = 0;
                 pixels[pixelindex].g = 0;
                 pixels[pixelindex].b = 0;
-            } */
+            }
 
         }
 
