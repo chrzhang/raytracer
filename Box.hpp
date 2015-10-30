fe65f98c4604ce1c6ac4eceacf0cedf9b19fa3b0
Christopher Zhang <christopherzhang3@gmail.com>
2015-10-30 23:46:03 +0000

    Use a slab-based Box implementation.

---
+++ Box.hpp
@@ -0,0 +1,27 @@
+#ifndef __BOX__
+#define __BOX__
+
+#include "Object.hpp"
+#include "Vector3D.hpp"
+#include "Ray3D.hpp"
+#include "Color.hpp"
+
+class Box : public Object {
+
+    Vector3D min, max;
+    Color color;
+
+    public:
+
+        Box();
+        Box(const Vector3D & min,
+            const Vector3D & max,
+            Color color);
+
+        virtual Color getColor() const;
+        virtual Vector3D getNormalAt(Vector3D point) const;
+        virtual double findIntersection(Ray3D ray) const;
+
+};
+
+#endif
